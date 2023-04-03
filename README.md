6e4e47c091f569e2f9fa91c792c8e0cdf8f30a87
mehdilaktaf <mehdi.laktaf@gmail.com>
2023-04-03 15:48:56 +0200

    ✨ added new route + fixed doc and readme

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Getting started with RPN Calculator API
 
-This project was created using python, fastapi and mongodb.
+This project was created using [python](https://www.python.org/), [fastapi](https://github.com/tiangolo/fastapi) and [mongodb](https://www.mongodb.com/fr-fr).
+<br>
 
 ## What is RPN ?
 
@@ -12,26 +13,34 @@ This is an example of an equation using the **Reverse Polish Notation** :
 What we normally use to write this equation is called the ***infix notation***, it looks like this :
 ### **`((1 + 2) / 3)`**
 
+<br>
+
 ## What can you use this calculator for?
 
 The goal of this API is to take in a RPN equation and compute it to give a result.
 You will also see the current time when calling the API, infix notation and of course the result.
 
-## Requirements
+<br>
 
+## Requirements
+<br>
 Now that I explained the RPN and goal of this application, here are the steps to follow to use this API:
+<br>
 
 * First, you will need to install all the requirements by lauching the following command:
 
     ### **`pip install requirements.txt`**
+<br>
 
 * Then you need to replace **`<database>`** and **`<uri>`** inside **`docker-compose.yml`**.
+<br><br>
 
-* You also need to add a  **`settings.py`** file in the project root and add the following variables:
+* /!\ You also need to add a  **`settings.py`** file in the project root and add the following variables: /!\ 
 
     ###  `MONGODB_URI = "your mongo db uri"`
     ###  `MONGODB_DB =  "your database"`
     ###  `MONGODB_COLLECTION = "your collection"`
+<br>
 
 * Once this is done, you should be able to launch the server using either:
 
@@ -40,7 +49,7 @@ Now that I explained the RPN and goal of this application, here are the steps to
     ## or
 
     ### `docker-compose up`
- 
+ <br>
 
 ## Usage
 
--- main.py
+++ main.py
@@ -1,15 +1,41 @@
+from typing import Annotated, List, Dict
 import csv
+
+from pydantic import BaseModel, validator
 import settings
 from io import StringIO
 from datetime import datetime
 from pymongo import MongoClient
-from fastapi import FastAPI, Response
+from fastapi import Body, FastAPI, HTTPException, Response
+from fastapi.responses import RedirectResponse
 from modules.rpn_calculator_console.rpn_calculator import *
 
 
-
+# Initiate application
 app = FastAPI()
 
+# Create custom schema for operation objects
+class Operation(BaseModel):
+    date_time: datetime
+    rpn: str
+    infix: str
+    result: int
+
+    @validator('date_time', pre=True)
+    def parse_date_time(cls, value):
+        if isinstance(value, str):
+            return datetime.strptime(value, '%m/%d/%Y %H:%M:%S')
+        return value
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "date_time": "01/01/2022 10:00:00",
+                "rpn": "2 2 +",
+                "infix": "2 + 2",
+                "result": 4
+            }
+        }
 
 # Use the URI in your code to connect to the MongoDB database
 client = MongoClient(settings.MONGODB_URI)
@@ -18,39 +44,62 @@ client = MongoClient(settings.MONGODB_URI)
 db =  client[settings.MONGODB_DB]
 rpn_collection = db[settings.MONGODB_COLLECTION]
 
+
 @app.get("/")
-def welcome():
-    return {"message": "Hello, please use curl or Postman to go to http://127.0.0.1:8000/rpn/ and enter a valid RPN equation that you want to compute. The request body should look like this : {'tokens' : 'equation'}"}
+def root():
+    '''
+    Redirects user to "/docs" swagger documentation.
+    '''
+    return RedirectResponse(url="/docs")
 
 # This is changed from a get to a post request to avoid division bug in url
-@app.post("/rpn/")
-def compute_rpn(tokens: dict):
+@app.post("/rpn", description="Returns the reverse polish notation equation in infix notation.", response_model=Operation)
+def compute_rpn(tokens: Annotated[Dict[str,str], Body(example={"tokens": "2 2 +"})]):
     '''
-    Returns the reverse polish notation equation in infix notation.
-
     Parameters:
-        tokens (dict): A dictionnary with "tokens" has the key and the equation as the value
+        - tokens (Dict[str, str]): A dictionnary with "tokens" has the key and the equation as the value
     Returns:
-        result (dict): Dictionnary representing the json response expected
+        - (Operation): The operation object created and added to the database.
     '''
     # Get result and infix equation equivalent
-    result = rpn_compute(tokens["tokens"])
-    infix = rpn_to_infix(tokens["tokens"])
+    try:
+        result = rpn_compute(tokens["tokens"])
+        infix = rpn_to_infix(tokens["tokens"])
+    except ValueError as e:
+        raise HTTPException(status_code=422, detail=str(e))
+    
     # Get current date_time
     now = datetime.now()
     date_time = now.strftime("%m/%d/%Y %H:%M:%S")
 
+    # Create new operation to add it to mongodb collection
+    op = Operation(date_time=date_time, rpn=tokens["tokens"], infix=infix, result=result)
+
     # Inserting data into MongoDB
-    rpn_collection.insert_one({"date_time": date_time, "rpn": tokens["tokens"], "infix": infix, "result": result})
+    rpn_collection.insert_one(op.dict())
 
-    return {"date_time": date_time, "rpn": tokens["tokens"], "infix": infix, "result": result}
+    # Return the operation created
+    return op.dict()
 
 
-@app.get("/operations")
+@app.get("/operations", description="Returns all operations in the database as JSON response.", response_model=List[Operation])
 def get_operations():
     '''
-    Returns all operations in the database as CSV format.
+    Returns:
+        response (list[Operations]): List of all Operations in database.
+    '''
+    # Get all operations from mongo and sort them by date_time in descending order
+    operations = []
+    for op in rpn_collection.find().sort("date_time", -1):
+        operation = Operation(**op)
+        operations.append(operation)
+    # Return the response object with the csv data and headers
+    return operations
 
+
+@app.get("/operations_csv", description="Returns all operations in the database as CSV format.", response_class=Response)
+def get_operations_as_csv():
+    '''
     Returns:
         response (Response): Response object with csv data and headers
     '''
@@ -74,3 +123,4 @@ def get_operations():
     }
     # Return the response object with the csv data and headers
     return Response(content=csv_data, headers=headers)
+
